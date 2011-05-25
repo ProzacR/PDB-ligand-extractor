6a1ee2d8765f7f15279dbe2f0490aa3f98b93a60
Vytautasrask <vytautas1987@yahoo.com>
2011-05-25 12:21:36 +0300

    more debug info, some fixes

--- isimti_ligandus.pl
+++ isimti_ligandus.pl
@@ -3,7 +3,7 @@
 #take ligands from pdb and save into pdb
 
 #here ignored ligands array
-@ismesti = (HOH, HG, ZN, CO, CU, NI, SO3, SO4, DOD, CL, OH, CO2);
+@ismesti = (HOH, HG, ZN, CO, CU, NI, SO3, SO4, DOD, CL, BR, OH, CO2, XE, SCN, NA, MN, RU);
 
 #main
 $x=0;
@@ -25,28 +25,29 @@ while (@lines[$n]) {
   if (($eilute[0] eq "HETATM") && !(grep {$_ eq $eilute[3]} @ismesti)) {
     if ($i == 0) {
       $paimtas_ligandas = $eilute[3];
+      print " paimtas ", $paimtas_ligandas;
+    }
+    if (($eilute[3] eq "A") || ($eilute[3] eq "B")) {
+    die "space error";
     }
     if ($eilute[3] ne $paimtas_ligandas) {
       writepdb();
-      $i = 0;
+      $i = -1;
     }
     @HETATM[$i] = @lines[$n]; #join(' ', @eilute); Avogadro likes empty spaces
     $i++;
-    } else {
-     if ($i != 0) {
-       writepdb();
-     }
-     $i = 0;
-   }
+    } 
   $n++;
 }
+writepdb();
 @lines = '';
 }
 
 sub writepdb {
-print " ", $paimtas_ligandas, ",";
+print " isaugomas,";
 $name = "${paimtas_ligandas}.pdb";
 open (FILE, ">$name");
 print FILE @HETATM;
+@HETATM = '';
 close(FILE);
 }
