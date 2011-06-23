884c36498bd73cdd03d70c07a34827b9e03c306a
ProzacR <vytautas1987@yahoo.com>
2011-06-23 04:16:35 -0700

    Su perlMol dabar turetu but viskas geriau...

--- isimti_ligandus.pl
+++ isimti_ligandus.pl
@@ -1,53 +1,36 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 #VR
 #take ligands from pdb and save into pdb
+#with PerlMol
 
-#here ignored ligands array
-@ismesti = (HOH, HG, ZN, CO, CU, NI, SO3, SO4, DOD, CL, BR, OH, CO2, XE, SCN, NA, MN, RU);
+use Chemistry::MacroMol;
+use Chemistry::File::PDB;
+
+#aminor. t.t.
+@ismesti = (" ZN", " HG", HOH, TRP, GLY, TYR, LYS, HIS, PRO, GLU, ASN, ASP, PHE, ILE, ALA,
+ARG, ASP, SER, ASN, VAL, GLN, THR, LEU, CYS, MET);
 
 #main
 $x=0;
 while (@ARGV[$x]) {
-open FILE, "<@ARGV[$x]" or die $!;
+$macromol = Chemistry::MacroMol->read("@ARGV[$x]") or die $!;
 print "\nFrom ", @ARGV[$x], ":";
-@lines = <FILE>;
 selectligands();
-close(FILE);
 $x++;
 }
 
 sub selectligands {
-$n=0;
-$i=0;
-while (@lines[$n]) {
-  @eilute = split(' ', @lines[$n]);
-  push(@eilute, "\n");
-  if (($eilute[0] eq "HETATM") && !(grep {$_ eq $eilute[3]} @ismesti)) {
-    if ($i == 0) {
-      $paimtas_ligandas = $eilute[3];
-      print " paimtas ", $paimtas_ligandas;
-    }
-    if (($eilute[3] eq "A") || ($eilute[3] eq "B")) {
-    die "space error";
-    }
-    if ($eilute[3] ne $paimtas_ligandas) {
-      writepdb();
-      $i = -1;
-    }
-    @HETATM[$i] = @lines[$n]; #join(' ', @eilute); Avogadro likes empty spaces
-    $i++;
-    } 
-  $n++;
-}
-writepdb();
-@lines = '';
-}
+@all_domains = $macromol->domains;
+$n = 0;
 
-sub writepdb {
-print " isaugomas,";
-$name = "${paimtas_ligandas}.pdb";
-open (FILE, ">$name");
-print FILE @HETATM;
-@HETATM = '';
-close(FILE);
+#visus reik isnagrineti ne tik 2 ir rasti ka imti ka ne
+while (@all_domains[$n]) {
+$tipas = @all_domains[$n]->type;
+if (!(grep {$_ eq $tipas} @ismesti)) {
+#taip galima israsyti ka reikia
+print $tipas, " ,";
+@all_domains[$n]->write("out.pdb", noid => 1);
+}
+$n++;
+}
 }
