2509a858e293091c64592c690b38439a5decdfde
ProzacR <vytautas1987@yahoo.com>
2011-06-28 01:14:07 -0700

    ups

--- isimti_ligandus.pl
+++ isimti_ligandus.pl
@@ -43,29 +43,3 @@ $n++;
 }
 print "\n";
 }
-
-#main
-$x=0;
-while (@ARGV[$x]) {
-$macromol = Chemistry::MacroMol->read("@ARGV[$x]") or die $!;
-print "\nFrom ", @ARGV[$x], ": ";
-selectligands();
-$x++;
-}
-
-sub selectligands {
-@all_domains = $macromol->domains;
-$n = 0;
-
-#visus domenus reikia isnagrineti ir rasti ka imti ka ne
-while (@all_domains[$n]) {
-$tipas = @all_domains[$n]->type;
-if (!(grep {$_ eq $tipas} @ismesti)) {
-#taip galima israsyti ka reikia
-print $tipas, "_", $n, " ";
-@all_domains[$n]->write("ligandai/${tipas}_${x}_${n}.pdb", noid => 1);
-}
-$n++;
-}
-print "\n";
-}
