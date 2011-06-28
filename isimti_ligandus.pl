aac05ad2249bbe26e7e24efc8b7e21138468e21c
ProzacR <vytautas1987@yahoo.com>
2011-06-28 01:09:24 -0700

    Ismesti list should be full now.

--- isimti_ligandus.pl
+++ isimti_ligandus.pl
@@ -8,14 +8,40 @@ use Chemistry::File::PDB;
 
 #aminor. t.t.
 @ismesti = (" ZN", " HG", DMS, HOH, SCN, SO3, SO4, DOD, H2S,
-NO3, XE, RU,
-MMC, AUC, IOD, GOL,
+NO3, " XE", " RU", "3CO", AZI, CCN, CMH, CNN, HGB, UNX, MBO,
+MMC, AUC, IOD, GOL, BE7,
 " BR", " CL", " CO", CO2, " CU", " MN", " NA", " NI", " OH",
 TRP, GLY, TYR, LYS, HIS, PRO, GLU, ASN, ASP, PHE, ILE, ALA,
 ARG, ASP, SER, ASN, VAL, GLN, THR, LEU, CYS, MET);
 
 if(!-d "ligandai") {
 mkdir("ligandai", 0777);
+}
+
+#main
+$x=0;
+while (@ARGV[$x]) {
+$macromol = Chemistry::MacroMol->read("@ARGV[$x]") or die $!;
+print "\nFrom ", @ARGV[$x], ": ";
+selectligands();
+$x++;
+}
+
+sub selectligands {
+@all_domains = $macromol->domains;
+$n = 0;
+
+#visus domenus reikia isnagrineti ir rasti ka imti ka ne
+while (@all_domains[$n]) {
+$tipas = @all_domains[$n]->type;
+if (!(grep {$_ eq $tipas} @ismesti)) {
+#taip galima israsyti ka reikia
+print $tipas, "_", $n, " ";
+@all_domains[$n]->write("ligandai/${tipas}_${x}_${n}.pdb", noid => 1);
+}
+$n++;
+}
+print "\n";
 }
 
 #main
